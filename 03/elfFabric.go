8af069d8aec8a7e1c240588c22b3dc509061691e
Michael K Welte Jr <mikewelte22@gmail.com>
2018-12-04 17:59:04 -0500

    Day 3 Part 2

--- 03/elfFabric.go
+++ 03/elfFabric.go
@@ -12,6 +12,7 @@ type elfFabric []string
 
 var graphPoints = [][]int{}
 var graph = [1000][1000]string{}
+var uniqueId int = 0
 
 func newFabricFromFile(filename string) elfFabric {
 	bs, err := ioutil.ReadFile(filename)
@@ -30,12 +31,18 @@ func (e elfFabric) print() {
 	}
 }
 
-func (e elfFabric) getSharedSquareInches() int {
-	graph = createGraph()
+func calculateUniqueness(e elfFabric) int {
+	uniqueID := 0
 
 	for _, f := range e {
 		fabric := strings.Split(f, " ")
-		//id := fabric[0]
+
+		id, err0 := strconv.Atoi(strings.Split(fabric[0], "#")[1])
+		if err0 != nil {
+			fmt.Println("Error:", err0)
+			os.Exit(1)
+		}
+
 		left, err1 := strconv.Atoi(strings.Split(fabric[2], ",")[0])
 
 		if err1 != nil {
@@ -62,29 +69,75 @@ func (e elfFabric) getSharedSquareInches() int {
 			os.Exit(1)
 		}
 
-		calculatePoints(left, top, length, height)
+		uniqueID = getUniqueID(left, top, length, height, id)
+
+		if uniqueID != 0 {
+			fmt.Println("UniqueId:", uniqueID)
+			return uniqueID
+		}
 	}
 
-	fmt.Println(len(graphPoints))
-	commonPoints := totalSquareInchesInCommon(graphPoints)
-	fmt.Println(commonPoints)
 	return 0
 }
 
-func calculatePoints(left int, top int, length int, height int) [][]int {
-	fmt.Println("Left:", left, "Top:", top, "Length:", length, "Height:", height)
+func (e elfFabric) getSharedSquareInches() int {
+	graph = createGraph()
+
+	for _, f := range e {
+		fabric := strings.Split(f, " ")
+
+		id, err0 := strconv.Atoi(strings.Split(fabric[0], "#")[1])
+		if err0 != nil {
+			fmt.Println("Error:", err0)
+			os.Exit(1)
+		}
+
+		left, err1 := strconv.Atoi(strings.Split(fabric[2], ",")[0])
+
+		if err1 != nil {
+			fmt.Println("Error:", err1)
+			os.Exit(1)
+		}
+
+		topStr := strings.Split(fabric[2], ",")[1]
+		top, err2 := strconv.Atoi(topStr[:len(topStr)-1])
+		if err2 != nil {
+			fmt.Println("Error:", err2)
+			os.Exit(1)
+		}
+
+		length, err3 := strconv.Atoi(strings.Split(fabric[3], "x")[0])
+		if err3 != nil {
+			fmt.Println("Error:", err3)
+			os.Exit(1)
+		}
+
+		height, err4 := strconv.Atoi(strings.Split(fabric[3], "x")[1])
+		if err4 != nil {
+			fmt.Println("Error", err4)
+			os.Exit(1)
+		}
+
+		calculatePoints(left, top, length, height, id)
+	}
+
+	commonPoints := totalSquareInchesInCommon(graphPoints)
+	calculateUniqueness(e)
+	return commonPoints
+}
+
+func calculatePoints(left int, top int, length int, height int, id int) [][]int {
 	for i := 0; i < length; i++ {
 		x := []int{}
 		y := []int{}
 		x = append(x, i+left)
+		x = append(x, id)
 		for j := 0; j < height; j++ {
 			y = append(y, j+top)
 		}
 		graphPoints = append(graphPoints, x)
 		graphPoints = append(graphPoints, y)
 	}
-
-	// 1,3 1,4 1,5 1,6 : 2,3 2,4 2,5 2,6
 
 	return graphPoints
 }
@@ -108,6 +161,23 @@ func totalSquareInchesInCommon(points [][]int) int {
 		}
 	}
 	return count
+}
+
+func getUniqueID(left int, top int, length int, height int, id int) int {
+	totalLength := length * height
+	uniqueCount := 0
+	for i := 0; i < length; i++ {
+		for j := 0; j < height; j++ {
+			if graph[left+i][j+top] == "#" {
+				uniqueCount++
+			}
+		}
+		if uniqueCount == totalLength {
+			return id
+		}
+	}
+
+	return 0
 }
 
 func createGraph() [1000][1000]string {
